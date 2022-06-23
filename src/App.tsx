9e7ce353c4114e4da93e240be068e3863d850497
JansenDF <jansen.lima.df@gmail.com>
2022-06-23 18:22:59 -0300

    Integrado com API graphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+  const GET_LESSONS_QUERY = gql `
+    query {
+      lessons {
+        id
+        title
+        teacher {
+          name
+        }
+      }
+    }
+  `
 function App() {
 
+  const { data } = useQuery(GET_LESSONS_QUERY)
+  console.log(data)
+
+  interface Lesson {
+    id: string;
+    title: string
+  }
+
   return (
-    <h1>Hello World</h1>
+    <ul>
+      {data?.lessons.map(
+        (lesson: Lesson) => {
+          return(
+            <li key={lesson.id}>{lesson.title}</li>
+          )
+        }
+      )}
+    </ul>  
   )
 }
 
